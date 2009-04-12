ca4b6aea7646676ab7a01975b54b8b2d8562a24b
adq_dvb <adq_dvb@lidskialf.net>
2009-04-12 18:48:50 +0000

    Document the EKT8210 code

--- pickit/elantech/EKT8210.py
+++ pickit/elantech/EKT8210.py
@@ -31,12 +31,16 @@ class EKT8210():
     def __init__(self, pickit):
 	"""Constructor.
 
-	args:
-	pickit -- pickit the touchpad is attached to - baud rate 9600."""
+	pickit: pickit the touchpad is attached to - using baud rate 9600."""
 
 	self.__pickit = pickit
 	
     def ReadNextPacket(self, timeout = None):
+	"""Read the next complete packet from the touchpad with an optional timeout.
+	
+	timeout: None for no timeout, or a positive float number of seconds.
+	Returns: the packet in a tuple, or () if the timeout expired."""
+
 	# Read first byte of packet
 	pkttype = self.__pickit.BufferedReadData(1, timeout)
 	if len(pkttype) == 0:
@@ -62,6 +66,11 @@ class EKT8210():
 	return data
 
     def ReadRegister(self, reg):
+	"""Read the contents of a touchpad register.
+
+	reg: Register to read.
+	Returns: 16 bit value."""
+
 	self.__pickit.WriteData((self.ETECH_PKT_READREG_REQ, reg << 4, 0x00, 0x01))
 	
 	# Wait for appropriate response
@@ -75,69 +84,139 @@ class EKT8210():
 	return (((data[1] << 16) | (data[2] << 8) | data[3]) >> 4) & 0xffff
 
     def WriteRegister(self, reg, value):
+	"""Write the contents of a touchpad register.
+
+	reg: Register to read.
+	value: 16 bit value."""
+
 	reg = reg & 0xf
 	value = value & 0xffff  
 	self.__pickit.WriteData((self.ETECH_PKT_WRITEREG, (reg << 4 | value >> 12) & 0xff, (value >> 4) & 0xff, (value << 4 | 0x01) & 0xff))
 
     def GetFirmwareVersion(self):
+	"""Get the version of the firmware.
+
+	Returns: tuple of (major, minor)."""
+
 	tmp = self.ReadRegister(self.ETECH_REG_FIRMWAREVERSION)
 	return (tmp >> 8, tmp & 0xff)
 
     def GetButtonStatus(self):
+	"""Get the current button status.
+	
+	Returns: The status (one bit per button)."""
+
 	return self.ReadRegister(self.ETECH_REG_BUTTONSTATUS)
 
     def GetXPosition(self):
+	"""Get the current pressed X position.
+
+	Returns: 16 bit x value."""
+
 	return self.ReadRegister(self.ETECH_REG_X)
 
     def GetYPosition(self):
+	"""Get the current pressed Y position.
+
+	Returns: 16 bit y value."""
+
 	return self.ReadRegister(self.ETECH_REG_Y)
 
     def GetSensitivity(self):
+	"""Get the sensitivity of the touchpad.
+
+	Returns: The sensitivity."""
+
 	return ReadRegister(self.ETECH_REG_SENSITIVITY) >> 12
     
     def SetSensitivity(self, sensitivity):
+	"""Set the sensitivity of the touchpad.
+
+	sensitivity: The sensitivity."""
+
 	self.WriteRegister(self.ETECH_REG_SENSITIVITY, (sensitivity & 0xf) << 12)
 
     def GetPowerState(self):
+	"""Get the power state of the touchpad.
+
+	Returns: "NORMAL" or "SLEEP"."""
+
 	return ["NORMAL", "SLEEP"][(self.ReadRegister(self.ETECH_REG_POWERSTATE) & 0x8000) != 0x8000]
     
     def SetPowerState(self, state):
+	"""Set the power state of the touchpad.
+
+	state: True -- normal, False -- sleep"""
+
 	self.WriteRegister(self.ETECH_REG_POWERSTATE, [0x0000, 0x8000][state == True])
 
     def GetTouchArea(self):
 	"""The pad appears to be divided up into discrete 2d areas, 
 	each of which has a unique id ranging from 0 -> 89. This 
-	returns the id of the currently touched area."""
+	returns the id of the currently touched area.
+
+
+	Returns: Area ID."""
 
 	return self.ReadRegister(self.ETECH_REG_TOUCHAREA) >> 8
 
     def GetPadSize(self, size):
+	"""Get the size of the pad.
+
+	Returns: Size."""
+
 	self.ReadRegister(self.ETECH_REG_PADSIZE) >> 12
 
     def SetPadSize(self, size):
 	"""This appears to control the size of the pad. 0 is 
-	the largest, whilst 15 is the smallest."""
+	the largest, whilst 15 is the smallest.
+
+	size: Size to set."""
+
 	self.WriteRegister(self.ETECH_REG_PADSIZE, size << 12)
 
     def GetAbsoluteMode(self):
+	"""Determine the mode of the touchpad.
+
+	Returns: True - absolute mode, False - relative mode."""
+
 	return (self.ReadRegister(self.ETECH_REG_MODE) & 0x8000) != 0x8000
     
     def SetAbsoluteMode(self, mode):
+	"""Set the touchpad into absolute mode.
+
+	mode: True for absolute mode, False for relative mode."""
+
 	self.WriteRegister(self.ETECH_REG_MODE, [0x8000, 0][mode == True])
 
     def GetReportRate(self):
+	"""Get the touchpad's reporting rate.
+    
+	Returns: the reporting rate."""
+
 	return self.ReadRegister(self.ETECH_REG_REPORTRATE) >> 12
 
     def SetReportRate(self, rate):
-	# 0 is fastest ... 5 is slowest
+	"""Set the touchpad's reporting rate (number of Report packets per second).
+	
+	rate: 0 is fastest, 5 is slowest."""
+
 	if rate > 5:
 	    rate = 5
 	self.WriteRegister(self.ETECH_REG_REPORTRATE, rate << 12)
     
     def GetFirmwareId(self):
+	"""Get the firmware ID.
+
+	Returns: The ID (should be 0x8210 for EKT8210)."""
+
 	return self.ReadRegister(self.ETECH_REG_FIRMWAREID)
 
     def ParseAbsoluteReportPacket(self, data):
+	"""Parse a report packet in absolute mode.
+
+	data: The 6 byte packet data.
+	Returns: tuple of (x, y, touch area, finger count, button state)."""
     
 	finger_count = (data[5] >> 1) & 3
 	buttons = data[5] >> 4
