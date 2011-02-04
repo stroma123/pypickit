96c1a4213140bc907b221b94ae25ba390ec27626
adq <adq@lidskialf.net>
2011-02-04 23:18:32 +0000

    Implement text output

--- python/analyser.py
+++ python/analyser.py
@@ -23,25 +23,49 @@ pickit.ClearReadBuffer()
 pickit.ClearWriteBuffer()
 pickit.ClearScriptBuffer()
 
-# enable voltages
-pickit.SetVppVoltage(3.3, 0.5)
-pickit.SetVddVoltage(3.3, 0.5)
+pickit.SetVddVoltage(3.3, 0.5) # FIXME
+pickit.SetVppVoltage(3.3, 0.5) # FIXME
 
+# Disable pickit power lines and set data pins as inputs
 s = PicKit2ScriptBuilder()
 s.SetProgrammingSpeed(0)
 s.ConfigureIcspPins(1,0,1,0)
 s.ConfigureAuxPin(1,0)
-
 s.VddGndOff()
 s.VddVoltageOff()
-
 s.VppPwmOff()
 s.VppGndOff()
-
-s.VppVoltageOn()
-s.VddVoltageOn()
-
+s.VppVoltageOn() # FIXME
+s.VddVoltageOn() # FIXME
 pickit.RunScriptImmediate(s)
 
+def outputPin(sample, oldSample, bit):
+    if (oldSample ^ sample) & bit:
+        sys.stdout.write("----------")
+    else:
+        if sample & bit:
+            sys.stdout.write("         |")
+        else:
+            sys.stdout.write("|         ")
 
-print pickit.LogicAnalyser(False, 1, 1, 0, 1, 1000, pickit.ANALYZER_SAMPLE_RATE_1KHZ)
+sampleRate = pickit.ANALYZER_SAMPLE_RATE_1KHZ
+samplePeriod = pickit.LogicAnalyserSamplePeriodMHz(sampleRate)
+
+print "   TIME   "  "    #     "  "   DAT    "  "    #     "  "   CLK    "  "    #     "  "   AUX    "
+oldSample = 0
+timeMhz = 0
+samples = pickit.LogicAnalyser(False, 1, 1, 0, 1, 1000, sampleRate)
+for x in xrange(0, len(samples)):
+    sample = samples[x]
+    sys.stdout.write("%10d" % timeMhz)
+    sys.stdout.write("    #     ")
+    outputPin(sample, oldSample, 1)
+    sys.stdout.write("    #     ")
+    outputPin(sample, oldSample, 2)
+    sys.stdout.write("    #     ")
+    outputPin(sample, oldSample, 4)
+    sys.stdout.write("    #     ")
+
+    timeMhz += samplePeriod
+    oldSample = sample
+    print 
